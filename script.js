78fd929a86dc2e23283b64bbbfc1e738ba93d790
Mustafa Unal <94565017+MustafaxUK@users.noreply.github.com>
2022-01-09 21:08:24 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict"
+const btn = document.querySelector(".switch-btn")
+const video = document.querySelector(".video-container")
+
+btn.addEventListener("click", () => {
+    if (!btn.classList.contains("slide")) {
+        btn.classList.add("slide")
+        video.pause();
+    } else {
+        btn.classList.remove("slide")
+        video.play()
+    }
+})
+
+const preloader = document.querySelector(".preloader")
+
+window.addEventListener("load", () => {
+    preloader.classList.add("hide-preloader")
+})
